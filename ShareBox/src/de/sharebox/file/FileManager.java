72380df67aae126b97c2b5c185840c340e5667b8
Rainforce15 <rainforce@gmx.net>
2013-08-15 14:30:46 +0200

    added required headers, implemented getUniqueInstance

--- ShareBox/src/de/sharebox/file/FileManager.java
+++ ShareBox/src/de/sharebox/file/FileManager.java
@@ -1,4 +1,24 @@
 package de.sharebox.file;
 
+import de.sharebox.file.model.FEntry;
+
 public class FileManager {
+
+    private static FileManager instance = new FileManager();
+
+    public static FileManager getUniqueInstance() {
+        return instance;
+	}
+
+    public boolean registerFEntry (FEntry f) {
+        return true;
+    }
+
+    public boolean pollAPIForChanges () {
+        return true;
+    }
+
+    public boolean pollFileSystemForChanges () {
+        return true;
+    }
 }
